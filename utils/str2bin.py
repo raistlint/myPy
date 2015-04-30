84586f0134fbea53afc2a2c9f05b99d670556dc9
rgao <831124titan@163.com>
2015-04-30 15:17:45 +0800

    update str2bin.py - add read binary file

--- utils/str2bin.py
+++ utils/str2bin.py
@@ -15,14 +15,32 @@ split hex sting to list, byte by byte
 """
 def cut(data):
     size = len(data);
-    print "size = ", size;
+    print "cut size = ", size;
     r = [];
     for i in range(0, size, 2):
         r.append(data[i:i+2])
     return r;
 
+"""
+split hex sting to lists, char
+"""
+def cutChar(data):
+    size = len(data)
+    print "size = ", size;
+    r = []
+    for i in range(0,size):
+        r.append(data[i:i+1])
+    return r;
+
+def display(a):
+    print "%02X" %ord(a),
+
 if __name__ == '__main__':
     data = "0EE5C897B6EA0151"
     fp = open("binFile", 'wb')
     map(lambda x: fp.write(struct.pack('B', string.atoi(x, 16))), cut(data))
+    fp.close()
+    
+    fp = open("ox8", 'rb')
+    map(display , cutChar(fp.read()))
     fp.close()
